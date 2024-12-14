b6d30d4e6a41c333de2476beac97ec030cbb8d37
yukiisen <ahmed.black4636@gmail.com>
2024-12-14 21:03:50 +0100

    wiw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
